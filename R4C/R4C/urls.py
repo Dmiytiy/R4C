40ddf481d90bb5eb7659b617f527ab40bf9cf58c
Dmiytiy <prostoy.general@gmail.com>
2023-09-27 21:55:53 +0300

    1) создал базу данных db.sqlite3 2) в models.py добавил метод, который определяет, что будет отображаться в виде строки 3)добавил путь по которуму отображается наше 1 задание  path api/robots/' 4)в views.py добавил 2 метода для добавления и отображения таблицы

--- R4C/R4C/urls.py
+++ R4C/R4C/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from robots.views import create_robot
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/robots/', create_robot, name='create-robot'),
 ]
--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,3 +1,37 @@
+
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Robot
+
+
+@csrf_exempt
+def create_robot(request):
+    #Добавление в базу данных при помощи метода POST
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+
+            model = data.get('model')
+            version = data.get('version')
+            created = data.get('created')
+
+            # Валидация данных здесь
+
+            robot = Robot(model=model, version=version, created=created)
+            robot.save()
+
+            return JsonResponse({'message': 'Робот успешно создан '})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+
+    elif request.method == 'GET':
+        # Обработка GET-запроса
+        robots = Robot.objects.all()
+        my_robots = [{'model': robot.model, 'version': robot.version, 'created': robot.created} for robot in
+                             robots]
+        return JsonResponse({'robots': my_robots})
+    return JsonResponse({'error': 'Недопустимый метод запроса'}, status=405)
